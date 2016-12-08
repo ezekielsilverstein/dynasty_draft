29d54210b1ee35cbd6fb86d6a703e518336d4bf8
ezekiel <ezekiel.m.silverstein@gmail.com>
2016-12-07 20:24:25 -0500

    Annotate probabilities_montecarlo.py

--- probabilities_montecarlo.py
+++ probabilities_montecarlo.py
@@ -4,6 +4,10 @@ import csv
 from argparse import ArgumentParser
 
 def set_standings(fname):
+    """
+    Import a textfile containing the standings of teams 
+    to be entered into the lottery
+    """
     with open(fname, 'rU') as f:
         standings = {}
         reader = csv.reader(f, delimiter=',')
@@ -14,12 +18,24 @@ def set_standings(fname):
     return standings
 
 def set_ball_count_dict():
+    """
+    Create a dictionary:
+
+    keys: team names
+    values: number of ping-pong balls
+    """
     counts = {}
     for t in sorted(standings.keys()):
         counts[standings[t]] = t - 6
     return counts
 
 def create_balls():
+    """
+    Create a list of ping-pong balls.
+    Each ball is labelled by a team name.
+    The number of balls for each team is set
+    by set_ball_count_dict()
+    """
     balls = []
     for k,v in counts.iteritems():
         for i in range(v):
@@ -27,6 +43,12 @@ def create_balls():
     return balls
 
 def selection():
+    """
+    Select ping-pong balls and create a draft order
+
+    Continue selecting teams which have not been chosen yet.
+    Result is a dictionary with the pick number and the team
+    """
     order = []
     while len(order) < 6:
         pick = random.choice(balls)
@@ -35,6 +57,14 @@ def selection():
     return order
 
 def CD(selection):
+    """
+    Create a dictionary to keep track of each simulation
+
+    keys: team name
+    values: another dictionary
+        v/k: pick number
+        v/v: occurences
+    """
     choices_dict = {}
     for t in sorted(standings.values()):
         choices_dict[t] = {i:0 for i in range(1,7)}
@@ -45,6 +75,14 @@ def CD(selection):
     return choices_dict
 
 def PD(choices_dict):
+    """
+    Copy the choices_dict from CD
+
+    Divide by the number of simulations
+    Returns a dictionary anologous to choices_dict,
+    but with the probabilities for each team receiving each pick
+    rather than the number of occurences throughout the simulations
+    """
     prob_dict = copy.deepcopy(choices_dict)
     for k,v in prob_dict.iteritems():
         for placement, count in v.iteritems():
@@ -52,6 +90,14 @@ def PD(choices_dict):
     return prob_dict
 
 def cumulative_PD(prob_dict):
+    """
+    Create a dictionary of cumulative probabilities
+
+    Similar structure of prob_dict from PD
+
+    values of each sub-dictionary contain
+    the probabilities of a team getting THAT PICK OR BETTER
+    """
     cum_prob_dict = {}
     for t in sorted(standings.values()):
         cum_prob_dict[t] = {i:0 for i in range(1,7)}
@@ -79,6 +125,9 @@ def printout(prob_dict, cum_prob_dict):
     return None
 
 def main():
+    """
+    Craft the total function
+    """
     global standings
     standings = set_standings(args.filename)
     global counts
