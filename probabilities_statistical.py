41ed530eaef401b99fb690e8fee26638380f21d5
ezekiel <ezekiel.m.silverstein@gmail.com>
2016-12-07 20:38:37 -0500

    Annotate probabilities_statistica.py

--- probabilities_statistical.py
+++ probabilities_statistical.py
@@ -5,6 +5,10 @@ from argparse import ArgumentParser
 from itertools import permutations
 
 def set_standings(fname):
+    """
+    Import a textfile containing the standings of teams 
+    to be entered into the lottery
+    """
     with open(fname, 'rU') as f:
         standings = {}
         reader = csv.reader(f, delimiter=',')
@@ -15,14 +19,32 @@ def set_standings(fname):
     return standings
 
 def set_ball_count_dict():
+    """
+    Create a dictionary:
+
+    keys: team names
+    values: number of ping-pong balls
+    """
     counts = {}
     for t in sorted(standings.keys()):
         counts[standings[t]] = t - 6
     return counts
 
 def permutation_odds(p):
+    """
+    Set the odds of a given permutation occuring
+
+    arg: an n=6 permutation of teams in the lottery
+
+    return: probability of permutation occuring
+    """
     chance_list = []
     denominator = 21
+    """
+    roll through each team in order
+    and calculate the probability of the next team getting chosen
+    multiply all at the end for a total probability for this permutation
+    """
     for t in p:
         numerator = counts[t]
         chance_of_selection = float(numerator)/denominator
@@ -32,20 +54,32 @@ def permutation_odds(p):
     return chance_of_this_permutation
 
 def create_prob_dict():
+    """
+    Create probability dictionary
+    of each team getting each pick
+    """
     odds = {}
     for t in standings.values():
         odds[t] = {i:0 for i in range(1,7)}
-    perms = permutations(standings.values())
-    for p in perms:
-        chance = permutation_odds(p)
-        for pick in range(len(p)):
-            odds[p[pick]][1+pick] += chance
-    for k,v in odds.iteritems():
+    perms = permutations(standings.values()) #Find permutations
+    for p in perms: #For each permutation
+        chance = permutation_odds(p) #Find the chance of each occuring
+        for pick in range(len(p)): #For each pick in the permutation
+            odds[p[pick]][1+pick] += chance #Add the probability the team getting the pick
+    for k,v in odds.iteritems(): #Round each probability
         for pick, chance in v.iteritems():
             odds[k][pick] = round(odds[k][pick],4)
     return odds
 
 def cumulative_PD(prob_dict):
+    """
+    Create a dictionary of cumulative probabilities
+
+    Similar structure of prob_dict from PD
+
+    values of each sub-dictionary contain
+    the probabilities of a team getting THAT PICK OR BETTER
+    """
     cum_prob_dict = {}
     for t in standings.values():
         cum_prob_dict[t] = {i:0 for i in range(1,7)}
@@ -73,6 +107,9 @@ def printout(prob_dict, cum_prob_dict):
     return None
 
 def main(fname):
+    """
+    Craft the total function
+    """
     global standings
     standings = set_standings(fname)
     global counts
