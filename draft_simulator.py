eca98a44298d379f19f1f9cdf36eccfaefb8ec7d
ezekielsilverstein <ezekiel.m.silverstein@gmail.com>
2017-05-12 15:38:20 -0400

    Draft Simulator Class incorporates statistical probability methods
    File takes additional `action` argument to perform lottery or return probabilities
    Renames simulator class file and montecarlo_probabilities file
    Removes redundant files original `draft_simulator.py`, `probabilities_statistical.py`

--- draft_simulator.py
+++ draft_simulator.py
@@ -2,90 +2,237 @@ import random
 import csv
 from time import sleep
 from argparse import ArgumentParser
+import datetime
+from itertools import permutations
 
-def set_standings(fname):
+class Simulator:
     """
-    Import a textfile containing the standings of teams 
-    to be entered into the lottery
+    Draft Lottery Simulation Class
     """
-    with open(fname, 'rU') as f:
-        standings = {}
-        reader = csv.reader(f, delimiter=',')
-        for row in reader:
-            place = int(row[0])
-            team = row[1]
-            standings[place] = team
-    return standings
+    def __init__(self, filename):
+        self.filename = filename
+        self.standings = self.read_in_standings()
 
-def set_ball_count_dict():
-    """
-    Create a dictionary:
+    def perform_lottery(self):
+        self.counts = self.set_ball_count()
+        self.ppballs = self.create_balls()
+        self.order = self.selection()
 
-    keys: team names
-    values: number of ping-pong balls
-    """
-    counts = {}
-    for t in sorted(standings.keys()):
-        counts[standings[t]] = t - 6
-    return counts
+    def read_in_standings(self):
+        """
+        Takes a filename, parses it
+        :return: standings dictionary
+        """
+        with open(self.filename, 'rU') as f:
+            standings = {}
+            reader = csv.reader(f, delimiter=',')
+            for row in reader:
+                place = int(row[0])
+                team = str(row[1])
+                standings[place] = team
+        return standings
 
-def create_balls():
-    """
-    Create a list of ping-pong balls.
-    Each ball is labelled by a team name.
-    The number of balls for each team is set
-    by set_ball_count_dict()
-    """
-    balls = []
-    for k,v in counts.iteritems():
-        for i in range(v):
-            balls.append(k)
-    return balls
+    def set_ball_count(self):
+        """
+        Sets the number of ping-pong balls for each team
+        :return: dictionary of counts 
+        """
+        counts = {}
+        for t in sorted(self.standings.keys()):
+            counts[self.standings[t]] = t - 6
+        return counts
 
-def selection():
-    """
-    Select ping-pong balls and create a draft order
+    def create_balls(self):
+        """
+        Create a list of ping-pong balls.
+        Each ball is labelled by a team name.
+        The nubmer of balls for each team is set
+        by the ball count dict
+        :return: list of ping-pong balls
+        """
+        ppballs = []
+        for k, v in self.counts.iteritems():
+            for i in range(v):
+                ppballs.append(k)
+        return ppballs
 
-    Continue selecting teams which have not been chosen yet.
-    Result is a dictionary with the pick number and the team
-    """
-    order = {}
-    while len(order) < 6:
-        pick = random.choice(balls)
-        if pick not in order.values():
-            order[len(order)+1]=pick
-    return order
+    def selection(self):
+        """
+        Select ping-pong balls and create a draft order
+        
+        Continue selecting teams which have not been chosen yet
+        Result is adictionary with the pick number and the team
+        :return: Lottery order dict
+        """
+        order = {}
+        while len(order) < 6:
+            pick = random.choice(self.ppballs)
+            if pick not in order.values():
+                order[len(order)+1]=pick
+        return order
 
-def printout(draft_order):
-    """
-    Printout to create suspension :)
-    """
-    for k in reversed(draft_order.keys()):
-        print "With Pick Number {}:".format(k)
-        sleep(2)
-        print draft_order[k]
-        print ""
-        sleep(2)
-    return None
+    def print_draft_results(self):
+        """
+        Printout to create suspension :)
+        """
+        current_year = datetime.datetime.now().year
+        print ("\nWelcome to the {} Hyuk Dynasty Rookie Draft\n"
+               .format(current_year))
 
-def main(fname):
+        for k in reversed(self.order.keys()):
+            print "With Pick Number {}:".format(k)
+            sleep(1)
+            print self.order[k]
+            print ""
+            sleep(1)
+        return None
+
+    ###
+    # Methods to calculate the odds of draft pick for each team
+    # PROBABILISTIC
+    ###
+
+    def calculate_probabilistic_odds(self):
+        """
+        Calculate both individual probabilities for a team getting a certain pick
+        AND getting a certain pick OR BETTER
+        :return: 
+        """
+        self.probabilistic_odds = self._calculate_individual_probabilities_statistical()
+        self.cumulative_probabilistic_odds = self._calculate_cumulative_probabilities_statistical()
+        return None
+
+    def _calculate_individual_probabilities_statistical(self):
+        """
+        Calculate the chance that each team gets a certain pick
+        :return: Nested probability dictionary
+        """
+        self.counts = self.set_ball_count()
+        standings_permutations = permutations(self.standings.values())
+        probabilistic_odds_dict = self._cycle_through_permutations(standings_permutations)
+        return probabilistic_odds_dict
+
+
+    def _calculate_cumulative_probabilities_statistical(self):
+        """
+        Calculate the chance that each team gets a certain pick OR BETTER
+        :return: Nested cumulative probability dictionary
+        """
+
+        # Create empty nested dictionary
+        pick_slot_dict = {i: 0 for i in range(1, 7)}
+        cumulative_probabilistic_odds_dict = {team: pick_slot_dict.copy() for team in self.standings.values()}
+
+        # For each team
+        for team in self.standings.values():
+            # Get the probabilistic values dict
+            pick_dict = self.probabilistic_odds[team]
+            for pick in pick_dict.keys():
+                cumulative_probabilistic_odds_dict[team][pick] = round(sum(
+                    [self.probabilistic_odds[team][i] for i in pick_dict.keys() if i <= pick]), 4)
+
+        return cumulative_probabilistic_odds_dict
+
+
+    def _cycle_through_permutations(self, permutations_gen):
+        """
+        Run through the permutations generator
+
+        :param permutations_gen: 
+        :return: A nested dictionary for each team and the chance of getting each pick
+        """
+
+        pick_slot_dict = {i: 0 for i in range(1, 7)}
+        choice_odds_dict = {team: pick_slot_dict.copy() for team in self.standings.values()}
+
+        # For each possible permutation
+        for permutation in permutations_gen:
+            # Get the chance of it occurring
+            chance = self._permutation_selection_chance(permutation)
+            # For each pick
+            for pick_num in range(len(permutation)):
+                # Increment the appropriate nested dict team-pick:chance-of-occurrence
+                team = permutation[pick_num]
+                choice_odds_dict[team][pick_num + 1] += chance
+
+        # Round the nested dict values
+        for team, pick_dict in choice_odds_dict.iteritems():
+            for pick in pick_dict.keys():
+                choice_odds_dict[team][pick] = round(choice_odds_dict[team][pick], 4)
+
+        return choice_odds_dict
+
+    def _permutation_selection_chance(self, permutation):
+        """
+        Returns the odds that a certain draft order
+        permutation will occur
+        :param permutation: A single draft order permutation
+        :return: Chance of selection
+        """
+
+        chance_list = []
+        denominator = sum(self.counts.values())
+
+        for t in permutation:
+            numerator = self.counts[t]
+            chance_of_selection = float(numerator)/denominator
+            chance_list.append(chance_of_selection)
+            denominator -= numerator
+
+        chance_of_this_permutation = reduce(lambda  x, y: x*y, chance_list)
+
+        return chance_of_this_permutation
+
+    def print_probabilities(self):
+        """
+        Print the team-pick probabilities
+        Both for a certain pick and for a certain pick OR BETTER
+        :return: None
+        """
+        for place in sorted(self.standings.keys()):
+            team = self.standings[place]
+            statement = (
+                "---{}--- ({}th place) has these probabilities:".format(team, place)
+            )
+            print statement
+            print "Pick\tThis pick\tThis pick or better"
+            for pick in sorted(self.probabilistic_odds[team].keys()):
+                print (
+                    "{}:\t{}\t\t{}".format(pick,
+                                           self.probabilistic_odds[team][pick],
+                                           self.cumulative_probabilistic_odds[team][pick]))
+                print ("")
+        return None
+
+
+
+def main(fname, action):
     """
-    Craft the total function
+    Set the Simulator Class and perform the lottery
+    :param fname: name of the standings txtfile
+    :param action: Perform the draft or calculate and print the odds
+    :return: completed Simulator Class 
     """
-    global standings
-    standings = set_standings(fname)
-    global counts
-    counts = set_ball_count_dict()
-    global balls
-    balls = create_balls()
-    draft_order = selection()
-    printout(draft_order)
-    return draft_order
-    
+    s = Simulator(fname)
+    s.calculate_probabilistic_odds() # Calculate odds regardless
+
+    if action == 'draft':
+        s.perform_lottery()
+        s.print_draft_results()
+    elif action == 'odds':
+        # Odds already calculated, just print them
+        s.print_probabilities()
+
+    return s
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-f', '--filename', default="lottery_standings_2016.csv",
-    type=str, help='Name of file containing lottery standings')
+                        type=str, help='Name of file containing lottery standings')
+    parser.add_argument('-a', '--action', default='draft', choices=['draft', 'odds'],
+                        type=str, help='Perform draft (default) or determine odds')
     args = parser.parse_args()
 
-    main(args.filename)
+    s = main(args.filename, args.action)
+
+
